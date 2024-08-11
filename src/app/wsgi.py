77e5832446ec4e8a95a29beb3ecf1a57541eed18
MAR2807 <105993122+MAR2807@users.noreply.github.com>
2024-08-11 16:27:37 -0400

    removed app.run

--- src/app/wsgi.py
+++ src/app/wsgi.py
@@ -4,8 +4,4 @@ from app import app
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == "__main__":
-    try:
-        app.run()
-    except Exception as e:
-        logging.error("An error occurred while running the app", exc_info=True)
+# No need to call app.run() in a serverless environment
